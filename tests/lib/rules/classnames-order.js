ec6ad1658e0c1d99f9b6a626ecb640f8f5028fb5
francoismassart <francois.massart@gmail.com>
2021-03-24 21:47:51 +0100

    MINOR

--- tests/lib/rules/classnames-order.js
+++ tests/lib/rules/classnames-order.js
@@ -27,21 +27,33 @@ var parserOptions = {
 
 var ruleTester = new RuleTester({parserOptions});
 
+var errors = [{
+  message: 'Invalid Tailwind CSS classenames order',
+}];
+
 ruleTester.run('classnames-order', rule, {
 
     valid: [
       {
-        code: `<div class="container box-content lg:box-border">...</div>`
-      }
+        code: `<div class="container box-content lg:box-border">...</div>`,
+      },
     ],
 
     invalid: [
         {
             code: `<div class="sm:w-6 container w-12">...</div>`,
-            output: `<div class="container sm:w-6 w-12">...</div>`,
-            errors: [{
-                message: 'Invalid Tailwind CSS classenames order',
-            }]
-        }
-    ]
+            output: `<div class="container w-12 sm:w-6">...</div>`,
+            errors: errors,
+        },
+        {
+            code: `<div class="sm:py-5 p-4 sm:px-7 lg:p-8">...</div>`,
+            output: `<div class="p-4 lg:p-8 sm:py-5 sm:px-7">...</div>`,
+            errors: errors,
+        },
+        {
+            code: `<div class="grid grid-cols-1 sm:grid-cols-2 sm:px-8 sm:py-12 sm:gap-x-8 md:py-16">...</div>`,
+            output: `<div class="p-4 lg:p-8 sm:py-5 sm:px-7">...</div>`,
+            errors: errors,
+        },
+    ],
 });
