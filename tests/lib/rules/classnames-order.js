ad99397f4f71bbe8e77f6fc5224e43f6d07473e8
francoismassart <francois.massart@gmail.com>
2021-03-25 10:27:22 +0100

    BUG Fixes and additional tests

--- tests/lib/rules/classnames-order.js
+++ tests/lib/rules/classnames-order.js
@@ -10,7 +10,7 @@
 
 var rule = require('../../../lib/rules/classnames-order'),
 
-    RuleTester = require('eslint').RuleTester;
+  RuleTester = require('eslint').RuleTester;
 
 
 //------------------------------------------------------------------------------
@@ -25,35 +25,65 @@ var parserOptions = {
   },
 };
 
-var ruleTester = new RuleTester({parserOptions});
+var ruleTester = new RuleTester({ parserOptions });
 
 var errors = [{
   message: 'Invalid Tailwind CSS classenames order',
 }];
 
 ruleTester.run('classnames-order', rule, {
-
-    valid: [
-      {
-        code: `<div class="container box-content lg:box-border">...</div>`,
-      },
-    ],
-
-    invalid: [
-        {
-            code: `<div class="sm:w-6 container w-12">...</div>`,
-            output: `<div class="container w-12 sm:w-6">...</div>`,
-            errors: errors,
-        },
-        {
-            code: `<div class="sm:py-5 p-4 sm:px-7 lg:p-8">...</div>`,
-            output: `<div class="p-4 lg:p-8 sm:py-5 sm:px-7">...</div>`,
-            errors: errors,
-        },
-        {
-            code: `<div class="grid grid-cols-1 sm:grid-cols-2 sm:px-8 sm:py-12 sm:gap-x-8 md:py-16">...</div>`,
-            output: `<div class="grid grid-cols-1 sm:grid-cols-2 sm:gap-x-8 sm:py-12 md:py-16 sm:px-8">...</div>`,
-            errors: errors,
-        },
-    ],
+  valid: [
+    {
+      code: `<div class="container box-content lg:box-border custom">Simple, basic</div>`,
+    },
+    {
+      code: `<div class="p-5 lg:p-4 md:py-2 sm:px-3 xl:px-6">'p', then 'py' then 'px'</div>`,
+    },
+    {
+      code: `<div class="custom another-custom w-12 lg:w-6">prependCustom: true</div>`,
+      options: [{
+        prependCustom: true,
+      }],
+    },
+  ],
+  invalid: [
+    {
+      code: `<div class="sm:w-6 container w-12">Classnames will be ordered</div>`,
+      output: `<div class="container w-12 sm:w-6">Classnames will be ordered</div>`,
+      errors: errors,
+    },
+    {
+      code: `<div class="sm:py-5 p-4 sm:px-7 lg:p-8">Enhancing readability</div>`,
+      output: `<div class="p-4 lg:p-8 sm:py-5 sm:px-7">Enhancing readability</div>`,
+      errors: errors,
+    },
+    {
+      code: `<div class="grid grid-cols-1 sm:grid-cols-2 sm:px-8 sm:py-12 sm:gap-x-8 md:py-16">:)</div>`,
+      output: `<div class="grid grid-cols-1 sm:grid-cols-2 sm:gap-x-8 sm:py-12 md:py-16 sm:px-8">:)</div>`,
+      errors: errors,
+    },
+    {
+      code: `<div class="bg-gradient-to-r from-green-400 to-blue-500 focus:from-pink-500 focus:to-yellow-500"></div>`,
+      output: `<div class="bg-gradient-to-r from-green-400 focus:from-pink-500 to-blue-500 focus:to-yellow-500"></div>`,
+      errors: errors,
+    },
+    {
+      code: `<div class="md:prose-2xl prose-xl prose sm:prose-sm"></div>`,
+      output: `<div class="prose prose-xl sm:prose-sm md:prose-2xl"></div>`,
+      errors: errors,
+    },
+    {
+      code: `<div class="sm:line-clamp-3 line-clamp-2"></div>`,
+      output: `<div class="line-clamp-2 sm:line-clamp-3"></div>`,
+      errors: errors,
+    },
+    {
+      options: [{
+        removeDuplicates: false,
+      }],
+      code: `<div class="w-12 lg:w-6 w-12">removeDuplicates: false</div>`,
+      output: `<div class="w-12 w-12 lg:w-6">removeDuplicates: false</div>`,
+      errors: errors,
+    },
+  ],
 });
