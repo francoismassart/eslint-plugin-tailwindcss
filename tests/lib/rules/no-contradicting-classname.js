5dc79e7a78b2c5b52d653a125b78092905a429db
francoismassart <francois.massart@gmail.com>
2021-08-17 19:08:35 +0200

    fix: #49 space-x-reverse can’t be used along with space-x-[key]

--- tests/lib/rules/no-contradicting-classname.js
+++ tests/lib/rules/no-contradicting-classname.js
@@ -119,6 +119,14 @@ ruleTester.run("no-contradicting-classname", rule, {
         },
       ],
     },
+    {
+      code: `
+      <div class="flex flex-row-reverse space-x-4 space-x-reverse">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>`,
+    },
   ],
 
   invalid: [
