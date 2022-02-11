6a5b05554d77ccb07b51b95808f7743c275b23e0
francoismassart <francois.massart@gmail.com>
2022-02-11 08:56:20 +0100

    FIX #121

--- tests/lib/rules/enforces-shorthand.js
+++ tests/lib/rules/enforces-shorthand.js
@@ -398,5 +398,25 @@ ruleTester.run("shorthands", rule, {
       output: `classnames({'md:!rounded-tr block md:rounded-tl md:rounded-b': true})`,
       errors: [generateError(["md:rounded-br", "md:rounded-bl"], "md:rounded-b")],
     },
+    {
+      code: `classnames('sfc-border-l-0 sfc-border-r-0')`,
+      output: `classnames('sfc-border-x-0')`,
+      options: [
+        {
+          config: { prefix: "sfc-" },
+        },
+      ],
+      errors: [generateError(["sfc-border-l-0", "sfc-border-r-0"], "sfc-border-x-0")],
+    },
+    {
+      code: `classnames('md_sfc-border-l-0 md_sfc-border-r-0')`,
+      output: `classnames('md_sfc-border-x-0')`,
+      options: [
+        {
+          config: { prefix: "sfc-", separator: "_" },
+        },
+      ],
+      errors: [generateError(["md_sfc-border-l-0", "md_sfc-border-r-0"], "md_sfc-border-x-0")],
+    },
   ],
 });
