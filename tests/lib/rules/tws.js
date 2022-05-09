34bc80ef3d50c5ddee3ad8f57d5aaa1f8a3bab70
francoismassart <francois.massart@gmail.com>
2022-05-09 09:01:35 +0200

    chore: demonstrate usage of tws callee

---
+++ tests/lib/rules/tws.js
@@ -0,0 +1,62 @@
+/**
+ * @fileoverview Detect classnames which do not belong to Tailwind CSS
+ * @author François Massart
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require("../../../lib/rules/no-custom-classname");
+var RuleTester = require("eslint").RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var parserOptions = {
+  ecmaVersion: 2019,
+  sourceType: "module",
+  ecmaFeatures: {
+    jsx: true,
+  },
+};
+
+var generateErrors = (classnames) => {
+  const errors = [];
+  if (typeof classnames === "string") {
+    classnames = classnames.split(" ");
+  }
+  classnames.map((classname) => {
+    errors.push({
+      messageId: "customClassnameDetected",
+      data: {
+        classname: classname,
+      },
+    });
+  });
+  return errors;
+};
+
+var ruleTester = new RuleTester({ parserOptions });
+
+ruleTester.run("no-custom-classname", rule, {
+  valid: [],
+
+  invalid: [
+    {
+      code: `
+      <div className={tws(\`classes-like this\`, someBoolean && \`more class-es\`)}>
+        classes-like this more class-es
+      </div>
+      `,
+      options: [
+        {
+          callees: ["tws"],
+        },
+      ],
+      errors: generateErrors("classes-like this more class-es"),
+    },
+  ],
+});
