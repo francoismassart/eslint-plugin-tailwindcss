ac4552319fa264847170dc387a93b29423c91395
francoismassart <francois.massart@gmail.com>
2023-01-20 10:58:24 +0100

    chore: add test for issue #197

--- tests/lib/rules/no-custom-classname.js
+++ tests/lib/rules/no-custom-classname.js
@@ -849,6 +849,33 @@ ruleTester.run("no-custom-classname", rule, {
         },
       ],
     },
+    {
+      code: `<div className="text-primary-500 text-xl">https://github.com/francoismassart/eslint-plugin-tailwindcss/issues/197</div>`,
+      options: [
+        {
+          config: {
+            theme: {
+              extend: {
+                colors: {
+                  primary: {
+                    50: "#fdf2f8",
+                    100: "#fce7f3",
+                    200: "#fbcfe8",
+                    300: "#f9a8d4",
+                    400: "#f472b6",
+                    500: "#ec4899",
+                    600: "#db2777",
+                    700: "#be185d",
+                    800: "#9d174d",
+                    900: "#831843",
+                  },
+                },
+              },
+            },
+          },
+        },
+      ],
+    },
   ],
 
   invalid: [
