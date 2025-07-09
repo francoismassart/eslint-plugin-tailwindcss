989eaa1378b47af5f57efadf1aa4ecdb2246a622
Francois Massart <francois.massart@gmail.com>
2025-07-09 20:34:38 +0200

    basic files setup (#382)
    * basic files setup
    * fix the workflows
    * add packageManager field
    * run on push only
    * docs: README
    * setting up internal eslint
    * add lint to workflows
    * docs: `defaultOptions` + TODO add configs

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { RuleModule } from "@typescript-eslint/utils/ts-eslint";
+import { ESLint } from "eslint";
+
+import { rules } from "./rules";
+
+type RuleKey = keyof typeof rules;
+
+interface Plugin extends Omit<ESLint.Plugin, "rules"> {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  rules: Record<RuleKey, RuleModule<any, any, any>>;
+}
+
+const { name, version } =
+  // `import`ing here would bypass the TSConfig's `"rootDir": "src"`
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  require("../package.json") as typeof import("../package.json");
+
+const plugin: Plugin = {
+  /**
+   * TODO: Add configs (recommended, etc.)
+   * @see https://github.com/typescript-eslint/examples/blob/main/packages/eslint-plugin-example-typed-linting/src/index.ts
+   */
+  meta: {
+    name,
+    version,
+  },
+  rules,
+};
+
+export = plugin;
