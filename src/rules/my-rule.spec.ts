6b91d0e139221adfd37f78fd66f87c174d72a84d
francoismassart <francois.massart@gmail.com>
2025-07-18 14:47:21 +0200

    test: add JSX test cases

--- src/rules/my-rule.spec.ts
+++ src/rules/my-rule.spec.ts
@@ -1,19 +1,18 @@
+import * as Parser from "@typescript-eslint/parser";
 import { RuleTester } from "@typescript-eslint/rule-tester";
 
 import { myRule, RULE_NAME } from "./my-rule";
 
-// const ruleTester = new RuleTester({
-//   languageOptions: {
-//     parser: require("@typescript-eslint/parser"),
-//     parserOptions: {
-//       ecmaFeatures: {
-//         jsx: true,
-//       },
-//     },
-//   },
-// });
-
-const ruleTester = new RuleTester();
+const ruleTester = new RuleTester({
+  languageOptions: {
+    parser: Parser,
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+  },
+});
 
 ruleTester.run(RULE_NAME, myRule, {
   valid: [
@@ -23,6 +22,9 @@ ruleTester.run(RULE_NAME, myRule, {
     },
     {
       code: `let y = 'abc123';`,
+    },
+    {
+      code: `<button onClick={() => { const name = 'John'; alert(name); }}>JSX</button>`,
     },
   ],
   invalid: [
@@ -39,6 +41,24 @@ ruleTester.run(RULE_NAME, myRule, {
             {
               messageId: "fix:const",
               output: `const z = 'foo'`,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      code: `<button onClick={() => { var name = 'John'; alert(name); }}>JSX</button>`,
+      errors: [
+        {
+          messageId: "issue:var",
+          suggestions: [
+            {
+              messageId: "fix:let",
+              output: `<button onClick={() => { let name = 'John'; alert(name); }}>JSX</button>`,
+            },
+            {
+              messageId: "fix:const",
+              output: `<button onClick={() => { const name = 'John'; alert(name); }}>JSX</button>`,
             },
           ],
         },
