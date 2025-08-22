9687e59e239864c08f9a5333bb51d1d808a2e79d
francoismassart <francois.massart@gmail.com>
2025-08-22 14:13:24 +0200

    lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
 import pluginJs from "@eslint/js";
 import importPlugin from "eslint-plugin-import";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
@@ -9,7 +12,14 @@ import tseslint from "typescript-eslint";
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { ignores: ["lib/**"] },
-  { languageOptions: { globals: globals.browser } },
+  {
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        tsconfigRootDir: path.dirname(fileURLToPath(import.meta.url)),
+      },
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
